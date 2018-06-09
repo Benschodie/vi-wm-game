b44ac8513d41a217f8c0895d056c686b206b8535
Benjamin Konopka <benjamin.konopka@virtual-identity.com>
2018-06-09 10:46:09 +0200

    adding table, delete # from url

---
+++ src/components/Vitable.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h2>Tabelle Virtual Identity</h2>
+        <table class="striped">
+            <thead>
+            <tr>
+                <th>Username</th>
+                <th>Punktzahl</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>Chris</td>
+                <td>83</td>
+            </tr>
+            <tr>
+                <td>Lukas</td>
+                <td>79</td>
+            </tr>
+            <tr>
+                <td>Anja</td>
+                <td>69</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vitable',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Vitable from '@/components/Vitable'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/vitable',
+      name: 'Vitable',
+      component: Vitable,
     }
   ]
 })
